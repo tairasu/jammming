e3784a0cfaf89ac4d1ab379f562b66294f627d14
aomizu <marco.tylus24@gmail.com>
2024-05-14 13:07:26 +0900

    adding and removing functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,64 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 import Tracklist from './components/Tracklist';
 import Playlist from './components/Playlist';
+import { useState } from 'react';
+
+//fake data with name, artist, album, and an id
+const tracks = [
+  {
+    name: 'Track 1',
+    artist: 'Artist 1',
+    album: 'Album 1',
+    id: 1,
+  },
+  {
+    name: 'Track 2',
+    artist: 'Artist 2',
+    album: 'Album 2',
+    id: 2,
+  },
+  {
+    name: 'Track 3',
+    artist: 'Artist 3',
+    album: 'Album 3',
+    id: 3,
+  },
+  {
+    name: 'Track 4',
+    artist: 'Artist 4',
+    album: 'Album 4',
+    id: 4,
+  },
+  {
+    name: 'Track 5',
+    artist: 'Artist 5',
+    album: 'Album 5',
+    id: 5,
+  },
+];
 
 function App() {
+  const [playlist, setPlaylist] = useState([]);
+
+  const addTrackToPlaylist = (track_id) => {
+    const track = tracks.find(t => t.id === track_id);
+    if (!playlist.includes(track)) {
+      setPlaylist([...playlist, track]);
+    }
+  };
+
+  const removeTrackFromPlaylist = (track_id) => {
+    const updatedPlaylist = playlist.filter(t => t.id !== track_id);
+    setPlaylist(updatedPlaylist);
+  }
+
   return (
     <div className="App bg-violet-100 h-screen">
       <div className="flex-col">
         <SearchBar />
         <div className="grid grid-cols-12">
-          <Tracklist />
-          <Playlist />
+          <Tracklist tracks={tracks} onAdd={addTrackToPlaylist} />
+          <Playlist tracks={playlist} onRemove={removeTrackFromPlaylist} />
         </div>
       </div>
     </div>
--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Track(props) {
+function PlaylistItem(props) {
     return (
         <div className="track p-4 border border-gray-300 rounded flex justify-between items-center">
             <div>
@@ -8,13 +8,13 @@ function Track(props) {
                 <p className="text-sm">{props.track.artist}</p>
             </div>
             <button
-                onClick={() => props.onAdd(props.track.id)}
-                className="p-2 bg-blue-500 text-white rounded"
+                onClick={() => props.onRemove(props.track.id)}
+                className="p-2 bg-red-500 text-white rounded"
             >
-                Add
+                Remove
             </button>
         </div>
     );
 }
 
-export default Track;
+export default PlaylistItem;
