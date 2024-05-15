66e0a59a4c79e5d486728194990648b33a00ad31
aomizu <marco.tylus24@gmail.com>
2024-05-15 19:51:55 +0900

    async fix

--- src/Spotify.js
+++ src/Spotify.js
@@ -26,7 +26,7 @@ const Spotify = {
     }
   },
 
-  search(term) {
+  async search(term) {
     const token = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&limit=5&q=${term}`, {
       headers: {
